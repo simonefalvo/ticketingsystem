0e6898166a4abc867b47ba250efcd2be92c4f06a
Andrea Ranfone <ranfone.andrea@gmail.com>
2018-05-26 15:59:03 +0200

    Implementata la modifica degli utenti tramite una modal ui

--- src/main/resources/static/lista-ticket/lista-ticket.component.js
+++ src/main/resources/static/lista-ticket/lista-ticket.component.js
@@ -1,38 +1,78 @@
 'use strict';
 
+angular.
+module('listaTicket').factory('myService',['$http','$location', function($http,$location) {
+
+    return{
+        getAll: function () {
+            return $http.get('ticket/').then(function(response) {
+                console.log("Success: " + response.statusText);
+                return response.data;
+            }, function (reason) {
+                console.log("Error: " + reason.statusText);
+            });
+        },
+        cancella: function ($utenteId) {
+            return $http.delete('ticket/' + $utenteId.toString()).
+            then(function (response) {
+                console.log("Success: " + response.statusText);
+            }, function (reason) {
+                console.log("Error: " + reason.statusText);
+            })
+        }
+    };
+}]);
+
 // Register `listaUtente` component, along with its associated controller and template
 angular.
 module('listaTicket').
 component('listaTicket', {
     templateUrl: 'lista-ticket/lista-ticket.template.html',
-    controller: ['$http', function listaTicketController($http) {
+    controller: ['$http','$location','$uibModal','$log','$document','myService', function listaTicketController($http,$location,$uibModal, $log, $document,myService) {
 
         var self = this;
 
+
         self.getAll = function () {
-            $http.get('ticket/').then(function(response) {
-                self.tickets = response.data;
-                console.log("Success: " + response.statusText);
-            }, function (reason) {
-                console.log("Error: " + reason.statusText);
+            myService.getAll().then(function(ticket) {
+                self.tickets = ticket;
             });
         };
 
         self.getAll();
 
-        self.visualizza = function (ticketId) {
-            $location.path('/dettagli-ticket/' + ticketId.toString());
-            //$http.get('ticket/' + ticketId.toString());
+        self.animationsEnabled = true;
+
+        self.open = function (id,assistenteid,customerid,categoria,descrizione,priorita_customer,priorita_team,prodotto,stato,teamid,time_stamp,titolo,oggetto_id,size, parentSelector) {
+            var items = [id,assistenteid,customerid,categoria,descrizione,priorita_customer,priorita_team,prodotto,stato,teamid,time_stamp,titolo,oggetto_id];
+            var parentElem = parentSelector ?
+                angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+            var modalInstance = $uibModal.open({
+                animation: self.animationsEnabled,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'dettagliTicket.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: '$modCtrl',
+                size: size,
+                appendTo: parentElem,
+                resolve: {
+                    items: function () {
+                        return items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                self.selected = selectedItem;
+                self.getAll();
+            }, function () {
+                self.getAll();
+            });
         };
 
-        self.elimina = function (ticketId) {
-            $http.delete('ticket/' + ticketId.toString()).
-                then(function (response) {
-                    self.getAll();
-                    console.log("Success: " + response.statusText);
-            }, function (reason) {
-                    console.log("Error: " + reason.statusText);
-            })
+        self.toggleAnimation = function () {
+            self.animationsEnabled = !self.animationsEnabled;
         };
 
     }]
