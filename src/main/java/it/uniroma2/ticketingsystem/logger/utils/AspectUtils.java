a81712fe3263e0ad7ad089a10f8b50774c70f936
smvfal <smvfal@gmail.com>
2018-06-21 15:24:20 +0200

    aggiunta funzione hasAnnotation in AspectUtils

--- src/main/java/it/uniroma2/ticketingsystem/logger/utils/AspectUtils.java
+++ src/main/java/it/uniroma2/ticketingsystem/logger/utils/AspectUtils.java
@@ -1,9 +1,16 @@
 package it.uniroma2.ticketingsystem.logger.utils;
 
+import java.lang.annotation.Annotation;
+
 public abstract class AspectUtils {
 
-    /*
-     *  Check if the option value has the default value
+    /**
+     * Check if the option of the annotation has the default value
+     *
+     * @param annotation the class of the annotation
+     * @param optionName the name of the option
+     * @param option the annotated object
+     * @return true if the option has the default value, false otherwise
      */
     public static boolean defaultOption(Class<?> annotation, String optionName, Object option) {
         Object defaultValue = null;
@@ -13,6 +20,21 @@ public abstract class AspectUtils {
             e.printStackTrace();
         }
         return option.equals(defaultValue);
+    }
+
+    /**
+     * Check if the object has the annotation
+     *
+     * @param object the object to be checked
+     * @param annotation the annotation class
+     * @return the annotation instance, null if there is not such annotation
+     */
+    public static Annotation hasAnnotation(Object object, Class<?> annotation) {
+        Annotation[] annotations = object.getClass().getAnnotations();
+        for (Annotation a : annotations)
+            if (a.annotationType().equals(annotation))
+                return a;
+        return null;
     }
 
 
