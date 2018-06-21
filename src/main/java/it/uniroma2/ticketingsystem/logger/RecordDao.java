458fd0f2f66c804caf943063db96fdbd6e9ccf1c
Aldo Pietrangeli <pietrangeli.aldo@gmail.com>
2018-06-21 14:38:17 +0200

    solved unset import

--- src/main/java/it/uniroma2/ticketingsystem/logger/RecordDao.java
+++ src/main/java/it/uniroma2/ticketingsystem/logger/RecordDao.java
@@ -3,7 +3,11 @@ package it.uniroma2.ticketingsystem.logger;
 import it.uniroma2.ticketingsystem.logger.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Repository
 public interface RecordDao extends JpaRepository<Record,Integer> {
